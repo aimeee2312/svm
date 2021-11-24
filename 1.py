fd9f1f760d97a5ef08b28ec58be2f4e5aa76b502
aimeee2312 <you@example.com>
2021-11-24 21:46:38 +0700

    kiem tra

---
+++ 1.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+np.random.seed(22)
+
+means = [[2, 2], [4, 2]]
+cov = [[.3, .2], [.2, .3]]
+N = 10
+X0 = np.random.multivariate_normal(means[0], cov, N)
+X1 = np.random.multivariate_normal(means[1], cov, N)
+X = np.concatenate((X0.T, X1.T), axis = 1)
+y = np.concatenate((np.ones((1, N)), -1*np.ones((1, N))), axis = 1) 
